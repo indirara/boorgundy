indirara/indirara.github.io

moving all here

src/app/buttons/see-packages/see-packages.component.ts
@@ -1,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-packages',
+  templateUrl: './see-packages.component.html',
+  styleUrls: ['./see-packages.component.scss'],
+})
+export class SeePackagesComponent implements OnInit {
+  text = 'SEE OUR PACKAGES';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  scrollToPackages(): void {
+    document.getElementById('packages').scrollIntoView({ behavior: 'smooth' });
+  }
+}

indirara/boorgundy

src/app/buttons/see-packages/see-packages.component.ts
@@ -1,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-packages',
+  templateUrl: './see-packages.component.html',
+})
+export class SeePackagesComponent implements OnInit {
+  text = 'SEE OUR PACKAGES';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  scrollToPackages(): void {
+    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
+  }
+}